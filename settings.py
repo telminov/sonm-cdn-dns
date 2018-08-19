b8a97ef4760fb9b618d1d65d1a27003b40f470d7
sergey@telminov.ru <sergey@telminov.ru>
2018-08-19 20:06:01 +0300

    поправки

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 NODE_MANAGER_URL = 'http://node-manager.cdn.sonm.soft-way.biz'
-NODE_MANAGER_TOKEN = '269f9cf80c75881adebd5db8a6782ca7b1c03f1a'
+NODE_MANAGER_TOKEN = '123'
 CDN_DOMAIN = 'cdn-sonm.soft-way.biz.'
-IP_STACK_ACCESS_KEY = 'd6b7f0fbe5b6e72d5b534b4989206cda'
+IP_STACK_ACCESS_KEY = '123'
